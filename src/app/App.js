c08326741dfeca53f84e54c11b8cf56de8101bcf
Tetiana Stupak <prokopik.ts@gmail.com>
2021-12-16 09:26:31 +0100

    check function for exist function

--- src/app/App.js
+++ src/app/App.js
@@ -5,9 +5,7 @@ import api from "./api";
 function App() {
     const [users, setUsers] = useState();
     useEffect(() => {
-        if (api.users.fetchAll) {
-            api.users.fetchAll().then((data) => setUsers(data));
-        };
+        api.users.fetchAll().then((data) => setUsers(data));
     }, []);
     const handleDelete = (userId) => {
         setUsers(users.filter((user) => user._id !== userId));
