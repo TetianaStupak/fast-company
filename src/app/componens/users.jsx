33e622250ada3c1ca144f9305a62dc791eae62a5
Tetiana Stupak <prokopik.ts@gmail.com>
2021-12-19 16:38:09 +0100

    Сотрировка_[КП] Универсальный TableHeader

--- src/app/componens/users.jsx
+++ src/app/componens/users.jsx
@@ -29,14 +29,7 @@ const Users = ({ users: allUsers, ...rest }) => {
         setCurrentPage(pageIndex);
     };
     const handleSort = (item) => {
-        if (sortBy.iter === item) {
-            setSortBy((prevState) => ({
-                ...prevState,
-                order: prevState.order === "asc" ? "desc" : "asc"
-            }));
-        } else {
-            setSortBy({ iter: item, order: "asc" });
-        };
+        setSortBy(item);
     };
 
     const filteredUsers = selectedProf
@@ -75,7 +68,14 @@ const Users = ({ users: allUsers, ...rest }) => {
             )}
             <div className="d-flex flex-column">
                 <SearchStatus length={count} />
-                {count > 0 && <UserTable users={usersCrop} onSort={handleSort} {...rest} />}
+                {count > 0 && (
+                    <UserTable
+                        users={usersCrop}
+                        onSort={handleSort}
+                        currentSort={sortBy}
+                        {...rest}
+                    />
+                )}
                 <div className="d-flex justify-content-center">
                     <Pagination
                         itemsCount={count}
