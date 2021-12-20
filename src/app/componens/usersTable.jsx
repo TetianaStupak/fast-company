062d61c19f8b3cb649c401f8c883afa8736cabc0
Tetiana Stupak <prokopik.ts@gmail.com>
2021-12-20 10:11:37 +0100

    Сортировка_[КП] Извлечение TableBody

--- src/app/componens/usersTable.jsx
+++ src/app/componens/usersTable.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import User from "./user";
+
 import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
 
 const UserTable = ({ users, onSort, selectedSort, ...rest }) => {
     const columns = {
-        name: { iter: "name", name: "Имя" },
+        name: { path: "name", name: "Имя" },
         qualities: { name: "Качество" },
-        professions: { iter: "profession.name", name: "Професии" },
-        completedMeetings: { iter: "completedMeetings", name: " Встретился, раз" },
-        rate: { iter: "rate", name: "Оценка" },
-        bookmark: { iter: "bookmark", name: "Избранное" },
+        professions: { path: "profession.name", name: "Професии" },
+        completedMeetings: { path: "completedMeetings", name: " Встретился, раз" },
+        rate: { path: "rate", name: "Оценка" },
+        bookmark: { path: "bookmark", name: "Избранное" },
         delete: {}
     };
     return (
         <table className="table">
             <TableHeader {...{ onSort, selectedSort, columns }} />
-            <tbody>
-                {users.map((user) => (
-                    <User {...rest} {...user} key={user._id} />
-                ))}
-            </tbody>
+            <TableBody {...{ columns, data: users }} />
         </table>
 
     );
