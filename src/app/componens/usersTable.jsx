b061e01327bef58bbcbd0578c4c11190cdc69ec9
Tetiana Stupak <prokopik.ts@gmail.com>
2021-12-20 09:44:05 +0100

    Сортировка_[КП] Извлечение TableHeader

--- src/app/componens/usersTable.jsx
+++ src/app/componens/usersTable.jsx
@@ -1,31 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import User from "./user";
+import TableHeader from "./tableHeader";
 
-const UserTable = ({ users, onSort, currentSort, ...rest }) => {
-    const handleSort = (item) => {
-        if (currentSort.iter === item) {
-            onSort({
-                ...currentSort,
-                order: currentSort.order === "asc" ? "desc" : "asc"
-            });
-        } else {
-            onSort({ iter: item, order: "asc" });
-        };
+const UserTable = ({ users, onSort, selectedSort, ...rest }) => {
+    const columns = {
+        name: { iter: "name", name: "Имя" },
+        qualities: { name: "Качество" },
+        professions: { iter: "profession.name", name: "Професии" },
+        completedMeetings: { iter: "completedMeetings", name: " Встретился, раз" },
+        rate: { iter: "rate", name: "Оценка" },
+        bookmark: { iter: "bookmark", name: "Избранное" },
+        delete: {}
     };
     return (
         <table className="table">
-            <thead>
-                <tr>
-                    <th onClick={() => handleSort("name")} scope="col">Имя</th>
-                    <th scope="col">Качества</th>
-                    <th onClick={() => handleSort("profession.name")} scope="col">Профессия</th>
-                    <th onClick={() => handleSort("completedMeetings")} scope="col">Встретился, раз</th>
-                    <th onClick={() => handleSort("rate")} scope="col">Оценка</th>
-                    <th onClick={() => handleSort("bookmark")} scope="col">Избранное</th>
-                    <th />
-                </tr>
-            </thead>
+            <TableHeader {...{ onSort, selectedSort, columns }} />
             <tbody>
                 {users.map((user) => (
                     <User {...rest} {...user} key={user._id} />
@@ -38,7 +28,7 @@ const UserTable = ({ users, onSort, currentSort, ...rest }) => {
 UserTable.propTypes = {
     users: PropTypes.array.isRequired,
     onSort: PropTypes.func.isRequired,
-    currentSort: PropTypes.object.inRequired
+    selectedSort: PropTypes.object.inRequired
 };
 
 export default UserTable;
