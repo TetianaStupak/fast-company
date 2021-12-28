49621f6f76fca7884ce3da49121528608761bfac
Tetiana Stupak <prokopik.ts@gmail.com>
2021-12-28 12:01:15 +0100

    [КП] NavBar

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import NavBar from "./components/navBar";
 import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
 
 function App() {
     return <div>
+        <NavBar />
         <Switch>
             <Route path="/users" component={Users} />
             <Route path="/login" component={Login} />
