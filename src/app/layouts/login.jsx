950ffa8e55dbd45e3d8a75037a111e46eeb03b97
Tetiana Stupak <prokopik.ts@gmail.com>
2022-01-10 09:16:12 +0100

    Radio_Обработка значений множества полей

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -3,23 +3,45 @@ import { useState } from "react/cjs/react.development";
 
 const Login = () => {
     const [email, setEmail] = useState("");
+
     const handleChange = (e) => {
         setEmail(e.target.value);
-        console.log(e.target.value);
+        console.log(e.target);
     };
     return (
         <form action="">
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email"
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
                     value={email}
                     onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                />
             </div>
-        </form>
+            <div>
+                <div>
+                    <label htmlFor="radio1">Radio 1</label>
+                    <input
+                        type="radio"
+                        id="radio1"
+                        name="radio"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="radio2">Radio 2</label>
+                    <input type="radio" id="radio2" name="radio" />
+                </div>
+            </div>
+        </form >
     );
 };
 
