5af2362bb32e9442af4b88d8d712e7d04b6b441a
Tetiana Stupak <prokopik.ts@gmail.com>
2022-01-17 08:58:51 +0100

    Radio

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -3,9 +3,15 @@ import { validator } from "../../utils/validator";
 import TextField from "../common/form/textField";
 import api from "../../api";
 import SelectField from "./../common/form/selectField";
+import RadioField from "../common/form/radioField";
 
 const RegisterForm = () => {
-    const [data, setDate] = useState({ email: "", password: "", profession: "" });
+    const [data, setDate] = useState({
+        email: "",
+        password: "",
+        profession: "",
+        sex: "male"
+    });
     const [professions, setProfession] = useState();
     const [errors, setErrors] = useState({});
     useEffect(() => {
@@ -88,6 +94,17 @@ const RegisterForm = () => {
                 value={data.profession}
                 error={errors.profession}
             />
+            <RadioField
+                options={[
+                    { name: "Male", value: "male" },
+                    { name: "Female", value: "female" },
+                    { name: "Other", value: "other" }
+                ]}
+                value={data.sex}
+                name="sex"
+                onChange={handleChange}
+            />
+
             <button
                 type="submit"
                 disabled={!isValid}
