4b996a6297774c16a80deff7437b60b395e0d915
Tetiana Stupak <prokopik.ts@gmail.com>
2022-02-15 09:58:03 +0100

    User Service

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -9,17 +9,17 @@ axios.interceptors.response.use(
         const expectedErrors =
             error.response &&
             error.response.status >= 400 &&
-            error.response.status < 500
+            error.response.status < 500;
         if (!expectedErrors) {
             console.log(error);
             toast.error("Something was wrong. Try it later");
         }
-        return Promise.reject(error)
+        return Promise.reject(error);
     });
 const httpService = {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete: axios.delete,
+    delete: axios.delete
 };
 export default httpService;
