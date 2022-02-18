cdc80770a901d511fc0a2edfbfa321ad1cd3c6d3
Tetiana Stupak <prokopik.ts@gmail.com>
2022-02-18 10:03:57 +0100

    Метод Sign Up (Регистрация)

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 const AuthContext = React.createContext();
 
@@ -8,8 +9,18 @@ export const useAuth = () => {
 };
 
 const AuthProvider = ({ children }) => {
+    async function signUp({ email, password }) {
+        const key = "AIzaSyCWZpuNQ3buyFgdrJC_qyqrBxWHV8fsBI0";
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
+        const data = await axios.post(url, {
+            email,
+            password,
+            returnSecureToken: true
+        });
+        console.log(data);
+    }
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{ signUp }}>
             {children}
         </AuthContext.Provider>
     );
