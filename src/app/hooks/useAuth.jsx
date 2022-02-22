2addd73c693ffdd865342a793a3e0562dcb1bb00
Tetiana Stupak <prokopik.ts@gmail.com>
2022-02-22 10:20:44 +0100

    Авторизация и аутентификация_Получение User data при загрузке

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { toast } from "react-toastify";
 import axios from "axios";
 import userService from "../services/user.service";
-import { setTokens } from "../services/localStorage.service";
+import localStorageService, { setTokens } from "../services/localStorage.service";
 
 const httpAuth = axios.create({
     baseURL: "https://identitytoolkit.googleapis.com/v1/",
@@ -32,6 +32,7 @@ const AuthProvider = ({ children }) => {
                 }
             );
             setTokens(data);
+            getUserData();
         } catch (error) {
             errorCatcher(error);
             const { code, message } = error.response.data.error;
@@ -95,6 +96,19 @@ const AuthProvider = ({ children }) => {
         const { message } = error.response.data;
         setError(message);
     }
+    async function getUserData() {
+        try {
+            const { content } = await userService.getCurrentUser();
+            setUser(content);
+        } catch (error) {
+            errorCatcher(error);
+        }
+    }
+    useEffect(() => {
+        if (localStorageService.getAccessToken()) {
+            getUserData();
+        }
+    }, []);
     useEffect(() => {
         if (error !== null) {
             toast(error);
