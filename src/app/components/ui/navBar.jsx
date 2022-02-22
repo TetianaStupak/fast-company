b7b4cb4d5b4fae9c2ae5b992e9185f4dc749c5d8
Tetiana Stupak <prokopik.ts@gmail.com>
2022-02-22 11:33:44 +0100

    Авторизация и аутентификация_Добавляем пользователя в NavBar

--- src/app/components/ui/navBar.jsx
+++ src/app/components/ui/navBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import NavProfile from "./navProfile";
 const NavBar = () => {
     const { currentUser } = useAuth();
     return (
@@ -21,11 +22,13 @@ const NavBar = () => {
                     )}
                 </ul>
                 <div className="d-flex">
-                    {currentUser ? <p>User</p>
-                        : <Link className="nav-link " aria-current="page" to="/login">
+                    {currentUser ? (
+                        <NavProfile />
+                    ) : (
+                        <Link className="nav-link " aria-current="page" to="/login">
                             Login
                         </Link>
-                    }
+                    )}
 
                 </div>
             </div>
