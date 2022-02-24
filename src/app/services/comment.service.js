0a429c5f74fe92e8b741956a28c595edb1c1c299
Tetiana Stupak <prokopik.ts@gmail.com>
2022-02-24 09:10:00 +0100

    Авторизация и аутентификация_Получение комментариев_Add rule FireBase

--- src/app/services/comment.service.js
+++ src/app/services/comment.service.js
@@ -9,6 +9,17 @@ const commentService = {
             payload
         );
         return data;
+    },
+    getComments: async (pageId) => {
+        const { data } = await httpService.get(
+            commentAndpoint, {
+            params: {
+                orderBy: '"pageId"',
+                equalTo: `"${pageId}"`
+            }
+        }
+        );
+        return data;
     }
 };
 export default commentService;
